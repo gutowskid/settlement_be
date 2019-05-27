2d2c859ff098b182ab38ea70d5f244a4eb24f674
Damian Gutowski <damian9550@gmail.com>
2019-05-27 09:57:06 +0200

    [PAY] update bill flow

--- payroll/payroll-rest/src/main/java/pl/edu/pw/mini/payroll/rest/bill/service/BillService.java
+++ payroll/payroll-rest/src/main/java/pl/edu/pw/mini/payroll/rest/bill/service/BillService.java
@@ -11,8 +11,9 @@ import pl.edu.pw.mini.payroll.api.bill.BillAccountNumberDto;
 import pl.edu.pw.mini.payroll.rest.bill.domain.Bill;
 import pl.edu.pw.mini.payroll.rest.bill.domain.BillRepository;
 
-import static pl.edu.pw.mini.payroll.rest.common.ErrorCode.PAY_0001;
-import static pl.edu.pw.mini.payroll.rest.common.ErrorCode.PAY_0002;
+import java.util.stream.Stream;
+
+import static pl.edu.pw.mini.payroll.rest.common.ErrorCode.*;
 
 @Service
 public class BillService {
@@ -42,11 +43,18 @@ public class BillService {
     }
 
     public BillAccountNumberDto getBillById(Long id) {
-        return assembler.toDto(repository.findById(id).orElseThrow(() -> PAY_0001));
+        Bill bill = repository.findById(id).orElseThrow(() -> PAY_0001);
+        if(Stream.of(BillStatus.SENT, BillStatus.PROCESSED).noneMatch(b -> b == bill.getStatus())) {
+            throw PAY_0007;
+        }
+        return assembler.toDto(bill);
     }
 
     public void markProcessedBill(Long id) {
         Bill bill = repository.findById(id).orElseThrow(() -> PAY_0002);
+        if(bill.getStatus() != BillStatus.ACCEPTED) {
+            throw PAY_0008;
+        }
         bill.setStatus(BillStatus.PROCESSED);
         bill.setProcessed(true);
         repository.save(bill);
